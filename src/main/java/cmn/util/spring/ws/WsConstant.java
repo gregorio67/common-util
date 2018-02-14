7ea288f24da31573fc3d240e41731b9777ffe2e1
gregorio67 <kkimdoy@gmail.com>
2018-02-14 10:51:00 +0900

    Create WsConstant.java

---
+++ src/main/java/cmn/util/spring/ws/WsConstant.java
@@ -0,0 +1,22 @@
+package cmn.util.spring.ws;
+
+public class WsConstants {
+
+	public static final String DEFAULT_WSSE_PREFIX = "wsse";
+
+	public static final String DEFAULT_WSSE_NAMESPACE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
+	
+	public static final String DEFAULT_WSSE_PASSWORD_NAMESPACE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText";
+	
+	public static final String DEFAULT_WSSE_WSU_NAMESPACE = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
+
+	public static final String DEFAULT_SOAP11_PREFIX = "soap";
+
+	public static final String DEFAULT_SOAP12_PREFIX = "soap";
+	
+	public static final String DEFAULT_WSSE_USERNAMETOKEN_TAG = "UsernameToken";
+	
+	public static final String DEFAULT_WSSE_USERNAME_TAG = "Username";
+	
+	public static final String DEFAULT_WSSE_PASSWORD_TAG = "Password";
+}
