a26cc7b27bdc221f8cda1bc93097c808749c7b70
gregorio67 <kkimdoy@gmail.com>
2018-03-15 14:31:08 +0900

    Create BlockService.java

---
+++ src/main/java/cmn/util/spring/service/BlockService.java
@@ -0,0 +1,8 @@
+public interface BlockService {
+
+	public void selectBlockService() throws Exception;
+	
+	public List<String> getBlockList() throws Exception;
+	
+	public void refresh() throws Exception;
+}
