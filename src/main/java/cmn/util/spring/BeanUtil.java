a6f894a79866254c0b07c9ba6d9cc97d8624e3a4
gregorio67 <kkimdoy@gmail.com>
2018-04-12 15:24:59 +0900

    Update BeanUtil.java

--- src/main/java/cmn/util/spring/BeanUtil.java
+++ src/main/java/cmn/util/spring/BeanUtil.java
@@ -24,4 +24,44 @@ public class BeanUtil extends BaseUtil{
 
 		return bean;
 	}
+	
+	/**
+	 * 
+	 *<pre>
+	 * 1.Description: Return WebApplicationContext
+	 * 2.Biz Logic:
+	 * 3.Author : LGCNS
+	 *</pre>
+	 * @return WebApplicationContext
+	 * @throws Exception
+	 */
+	
+	public static WebApplicationContext getContext() throws Exception {
+		return ContextLoader.getCurrentWebApplicationContext();
+	}
+	
+	/**
+	 * 
+	 *<pre>
+	 * 1.Description: Return Current Active profiles
+	 * 2.Biz Logic:
+	 * 3.Author : LGCNS
+	 *</pre>
+	 * @return String[]
+	 * @throws Exception
+	 */
+	public static String[] getActiveProfile() throws Exception {
+		return getContext().getEnvironment().getActiveProfiles();
+	}
+	
+
+	public static InputStream getResoure(String resourceName) throws Exception {
+		char[] charName = resourceName.toCharArray();
+		if (charName[0] == File.pathSeparatorChar) {
+			resourceName = resourceName.substring(1);
+		}
+		resourceName = ResourceUtils.CLASSPATH_URL_PREFIX + resourceName;
+		return new FileInputStream(ResourceUtils.getFile(resourceName));
+	}
+	
 }
