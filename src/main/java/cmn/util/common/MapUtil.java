313484197cbd39ec6b7e7343b5caf788fd3d69d7
gregorio67 <kkimdoy@gmail.com>
2018-04-13 14:59:32 +0900

    Update MapUtil.java

--- src/main/java/cmn/util/common/MapUtil.java
+++ src/main/java/cmn/util/common/MapUtil.java
@@ -1,16 +1,20 @@
-import java.io.Serializable;
+	
+	mport java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
-import org.apache.commons.lang3.SerializationUtils;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
+import stis.framework.converter.JsonUtil;
 import stis.framework.vo.TestVo;
 
 
@@ -49,6 +53,8 @@ public class MapUtil {
 		return sb.toString();
 	}
 	
+	/**
+	 *  
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static Map setMapToMap(Map sourceMap) throws Exception {
@@ -70,54 +76,175 @@ public class MapUtil {
 		return targetMap != null && targetMap.size() > 0 ? targetMap : null;
 	}
 	
+	/**
+	 * 
+	 *<pre>
+	 * 1.Description: Convert map to value object
+	 * 2.Biz Logic:
+	 * 3.Author : LGCNS
+	 *</pre>
+	 * @param map source map
+	 * @param clazz target value object class
+	 * @return value object
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T map2Object(Map<String, Object> map, Class<?> clazz) throws Exception {
+		return (T) map2Object(map, clazz.newInstance());
+	}
+
+	/**
+	 * 
+	 *<pre>
+	 * 1.Description: Convert map to value object
+	 * 2.Biz Logic:
+	 * 3.Author : LGCNS
+	 *</pre>
+	 * @param map source map
+	 * @param vo target value object
+	 * @return value object
+	 * @throws Exception
+	 */
+	public static <T> T map2Object(Map<String, Object> map, T vo) throws Exception {
+		
+		Set<Entry<String, Object>> mapSet = map.entrySet();
+		Iterator<Entry<String, Object>> itr = mapSet.iterator();
+		
+		while(itr.hasNext()) {
+			String key = itr.next().getKey();
+			if (key.contains("serialVersionUID")) 
+				continue;
+			FieldUtils.writeField(vo, key, map.get(key), true);
+		}
+		
+		return (T) vo;
+	}
+
+	/**
+	 *
+	 *<pre>
+	 * Convert Value Object to Map
+	 *</pre>
+	 * @param obj Class
+	 * @return Map<String, Object>
+	 */
+	public static<T> Map<String, Object> object2Map(T clazz)throws Exception {
+		Map<String, Object> retMap = new HashMap<String, Object>();
+
+		try {
+			 Field[] fields = FieldUtils.getAllFields(clazz.getClass());
+			   for (Field field : fields) {
+				 	String name =field.getName();
+					Object object = FieldUtils.readField(clazz, name, true);
+					retMap.put(name, object);
+			   }			 
+		}
+		catch(Exception ex) {
+		}
+		return retMap;
+	}
+	
 	
 	/**
 	 * 
 	 *<pre>
-	 * 1.Description: Copy map
+	 * 1.Description: Merge source map to target map
+	 *                if target map is null, create target map;
 	 * 2.Biz Logic:
 	 * 3.Author : LGCNS
 	 *</pre>
-	 * @param map
+	 * @param sourceMap
+	 * @param targetMap
 	 * @return
 	 * @throws Exception
 	 */
-	public static Map<String, Object> copyMap(Map<String, Object> map) throws Exception {
-		
-		String json = new Gson().toJson(map);
-		Map<String, Object> resultMap = new Gson().fromJson(json, new TypeToken<Map<String, Object>>() {}.getType());
-		
-		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug("orignal Map :: {}", map.toString());
-			LOGGER.debug("copied Map :: {}", resultMap.toString());
-			
-		}
-		
-		return resultMap != null ? resultMap : null;
+	public static Map<String, Object> mergeMap(Map<String, Object> sourceMap,  Map<String, Object> targetMap) throws Exception {
+		return mergeMap(sourceMap, targetMap, null);
 	}
 	
-	public static <T extends Serializable> T copyObject(T clazz) throws Exception {
-		T copiedObj =  (T)SerializationUtils.clone(clazz);
+	/**
+	 * 
+	 *<pre>
+	 * 1.Description: Merge source map to target map
+	 *                if target map is null, create target map;
+	 * 2.Biz Logic:
+	 * 3.Author : LGCNS
+	 *</pre>
+	 * @param sourceMap source map
+	 * @param targetMap target map
+	 * @param args key name of source map for merging to target map
+	 * @return map
+	 * @throws Exception
+	 */
+	public static Map<String, Object> mergeMap(Map<String, Object> sourceMap,  Map<String, Object> targetMap, String[] args) throws Exception {
+		if (targetMap == null) {
+			targetMap = new HashMap<String, Object>();
+		}
 		
-		return copiedObj;
+		Set<Entry<String, Object>> sourceSet = sourceMap.entrySet();
+		Iterator<Entry<String, Object>> sourceItr = sourceSet.iterator();
 		
+		
+		/** Add data from sourcMap to targetMap **/
+		while(sourceItr.hasNext()) {
+			String key = sourceItr.next().getKey();
+			if (args != null) {
+				for (String arg : args) {
+					if (key.equals(arg)) {
+						targetMap.put(key, sourceMap.get(key));										
+					}
+				}
+			}
+			else {
+				targetMap.put(key, sourceMap.get(key));				
+			}
+		}
+		return targetMap;
 	}	
+	/**
+	 * 
+	 *<pre>
+	 * 1.Description: Convert HttpRequest payload to Map
+	 * 2.Biz Logic:
+	 * 3.Author : LGCNS
+	 *</pre>
+	 * @param request http servlet request
+	 * @return map 
+	 * @throws Exception
+	 */
+	@SuppressWarnings("unchecked")
+	public static Map<String, Object> httpRequest2Map(HttpServletRequest request) throws Exception {
+		/** Get Input stream from HttpServletRequest **/
+		InputStream is = request.getInputStream();
+
+		/** Read data from InputStream **/
+		byte[] requestData = IOUtils.toByteArray(is);
+		
+		String strParam = requestData != null ? new String(requestData) : null;
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("Request Payload Data :: {}", strParam);
+		}
+		return (Map<String, Object>) (strParam != null ? JsonUtil.json2Map(strParam) : new HashMap<String, Object>());
+
+	}
+	
 	
 	public static void main(String args[]) throws Exception {
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("name", "kkimdoy");
-		map.put("age", 10);
-		
-		copyMap(map);
-		
-		
-		TestVo vo = new TestVo();
-		vo.setAge(10);
-		vo.setName("kkimdoy");
-		vo.setPassword("password");
-		
-		TestVo vo1 = copyObject(vo);
-		
-		LOGGER.debug("{} :: {} :: {}", vo1.getName(), vo1.getPassword(), vo1.getAge());
+	   TestVo vo = new TestVo();
+	   vo.setAge(10);
+	   vo.setName("kkimdoy");
+	   vo.setPassword("aaaaa");
+	   
+	   Map<String, Object> map = object2Map(vo);
+	   LOGGER.debug("map :: {}", map);
+	   
+	   TestVo vo1 = new TestVo();
+	   map2Object(map, vo1);
+	   LOGGER.debug("name :: {}", vo1.getName());
+
+	   
+	   TestVo vo2 = map2Object(map, TestVo.class);
+	   LOGGER.debug("name :: {}", vo2.getName());
+
 	}
 }
