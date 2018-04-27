0d255af091cf9f5d388cda3b35c1de7f734796f0
gregorio67 <kkimdoy@gmail.com>
2018-04-27 10:41:52 +0900

    Create AuthCheckServiceImpl.java

---
+++ src/main/java/cmn/util/spring/service/impl/AuthCheckServiceImpl.java
@@ -0,0 +1,51 @@
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import stis.framework.base.BaseService;
+import stis.framework.spring.service.AuthCheckService;
+
+
+public class AuthCheckServiceImpl extends BaseService implements AuthCheckService {
+
+	/** JDBC Template **/
+	private JdbcTemplate jdbcTemplate;
+	
+	/** SQL for checking authorization **/
+	private String defaultSql;
+	
+	/** Result column name from SQL **/
+	private String resultColName;
+	
+	@Required
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Required
+	public void setDefaultSql(String defaultSql) {
+		this.defaultSql = defaultSql;
+	}
+
+	@Required
+	public void setResultColName(String resultColName) {
+		this.resultColName = resultColName;
+	}
+
+	@Override
+	public int selectAuthCheck(Object[] params) throws Exception {
+		
+		Map<String, Object> resultMap = jdbcTemplate.queryForMap(defaultSql, params);
+		return resultMap.get(resultColName) != null ? Integer.parseInt(String.valueOf(resultMap.get(resultColName))) : 0;
+	}
+	
+}
+
+/**
+    <bean id="authCheckService" class="stis.framework.spring.service.impl.AuthCheckServiceImpl">
+		<property name="jdbcTemplate" ref="jdbcTemplate" />
+		<property name="defaultSql" value="#{system['auth.check.sql']}" />
+		<property name="resultColName" value="cnt" />
+	</bean>
+**/  
